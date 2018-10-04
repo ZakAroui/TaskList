2e5ab0c0f908c875e59f5c318b033b66ee4b0175
Zak <zaki.aroui@gmail.com>
2018-10-03 23:32:55 -0400

    added permissionsService

---
+++ app/src/main/java/com/ikazme/tasklist/service/PermissionsService.java
@@ -0,0 +1,54 @@
+package com.ikazme.tasklist.service;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by ikazme on 9/3/18.
+ */
+
+public class PermissionsService {
+
+
+    private static PermissionsService permissionsService;
+
+    private PermissionsService() {
+    }
+
+    public static PermissionsService getInstance(){
+        if (permissionsService == null){
+            permissionsService = new PermissionsService();
+        }
+        return permissionsService;
+    }
+
+
+
+
+    public boolean hasOrRequestRecordAudioPerm(AppCompatActivity activity, int reqCode){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && activity.checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            activity.requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, reqCode);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean hasRecordAudioPerm(AppCompatActivity activity){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && activity.checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+
+}
